ef159604f9b7dc47c629f98957ea0f28c5dbcc3e
zivile.utkeviciute <zivile.utkeviciute@agillic.com>
2020-10-14 13:02:32 +0200

    Added loading indicator and fixed breaking layout

--- src/components/dataChart/DataChart.js
+++ src/components/dataChart/DataChart.js
@@ -1,49 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import React, { useState, useEffect, Fragment } from 'react'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import CustomTooltip from './customTooltip/CustomTooltip'
 import { mapData } from './dataChart.utility'
 import './dataChart.css'
 
-
 const DataChart = ({ latitude, longitude }) => {
   const apiUrl = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${latitude}&lon=${longitude}`
 
-  const [appState, setAppState] = useState({
+  const [dataFetch, setDataFetch] = useState({
     loading: false,
     data: null,
     error: null
-  });
+  })
 
   useEffect(() => {
-    setAppState({loading: true});
+    let unmounted = false
+    setDataFetch({loading: true})
+
     fetch(apiUrl)
       .then((res) => res.json())
       .then((data) => {
-        const filteredData = mapData(data.properties.timeseries)
-        const next24h = filteredData.slice(0, 24)
-        setAppState({loading: false, data: next24h})
+        if (!unmounted) {
+          const filteredData = mapData(data.properties.timeseries)
+          const next12h = filteredData.slice(0, 12)
+          setDataFetch({loading: false, data: next12h})
+        }
       }).catch(() => {
-        setAppState({error: 'Failed to retrieve data'})
-      });
-  }, [setAppState, apiUrl]);
+        if (!unmounted) {
+          setDataFetch({error: 'Failed to retrieve data', loading: false})
+        }
+      })
+
+    return () => unmounted = true
+
+  }, [setDataFetch, apiUrl])
 
   return (
     <div className='dataChart'>
-      <h3>Wind speed for the next 24h</h3>
-      <LineChart width={730} height={250} data={appState.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='time' />
-        <YAxis />
-        <Tooltip content={<CustomTooltip />} />
-        <Line type='monotone' dataKey='windSpeed' stroke='#75CFF0' strokeWidth='2' activeDot={{ r: 7 }} />
-      </LineChart>
-      <div className='legend'>
-        <span>Bicycle paths:</span>
-        <span>Regular</span>
-        <span>Green</span>
-      </div>
+      {dataFetch.error && <div className='error'>{dataFetch.error}</div>}
+      {dataFetch.loading
+        ? <CircularProgress />
+        : (
+          <Fragment>
+            <h3>Wind speed for the next 12h</h3>
+            <LineChart width={650} height={200} data={dataFetch.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+              <CartesianGrid strokeDasharray='3 3' />
+              <XAxis dataKey='time' />
+              <YAxis />
+              <Tooltip content={<CustomTooltip />} />
+              <Line type='monotone' dataKey='windSpeed' stroke='#75CFF0' strokeWidth='2' activeDot={{ r: 7 }} />
+            </LineChart>
+            <div className='legend'>
+              <span>Bicycle paths:</span>
+              <span>Regular</span>
+              <span>Green</span>
+            </div>
+          </Fragment>
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default DataChart
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import MapGL, { Source, Layer , Marker} from 'react-map-gl'
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';import DataChart from '../dataChart/DataChart'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import DataChart from '../dataChart/DataChart'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './Map.css'
 
 const token ='pk.eyJ1Ijoieml2aWxldSIsImEiOiJja2c2aHRmMDAwZW95Mndtb2lyamRkcXFnIn0.ux3wBgiUgLiF4DXA3ogXZQ'
-const apiUrl = `https://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:cykeldata_kk&outputFormat=json&SRSNAME=EPSG:4326`;
+const apiUrl = `https://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:cykeldata_kk&outputFormat=json&SRSNAME=EPSG:4326`
 
 const Map = () => {
   const [viewport, setViewPort] = useState({
@@ -14,13 +16,13 @@ const Map = () => {
     latitude: 55.6961,
     longitude: 12.4800,
     zoom: 11
-  });
+  })
 
   const [dataFetch, setDataFetch] = useState({
     loading: false,
     data: null,
     error: null
-  });
+  })
 
   const [coordinates, setCoordinates] = useState({
     latitude: 55.6961,
@@ -36,63 +38,70 @@ const Map = () => {
       .then((data) => {
         setDataFetch({loading: false, data: data})
       }).catch(() => {
-        setDataFetch({error: 'Failed to retrieve data'})
-      });
-    }, [setDataFetch]);
+        setDataFetch({error: 'Failed to retrieve data', loading: false})
+      })
+  }, [setDataFetch])
 
   const greenRoutes = dataFetch.data?.features.filter(entry => entry.properties.under_kategori === 'Grøn')
 
   return (
     <div className='mapWrapper'>
       {dataFetch.error && <div className='error'>{dataFetch.error}</div>}
-      <MapGL
-        {...viewport}
-        mapboxApiAccessToken={token}
-        mapStyle='mapbox://styles/mapbox/streets-v11'
-        onViewportChange={onViewportChange}
-        onNativeClick={e => setCoordinates({latitude: e.lngLat[0], longitude: e.lngLat[1]})}
-      >
-        <Marker latitude={coordinates.latitude} longitude={coordinates.longitude} >
-          <FiberManualRecordIcon style={{ color: 'crimson', fontSize: 32, cursor: 'pointer' }} />
-        </Marker>
-        {dataFetch.data &&
+      {dataFetch.loading
+        ? <CircularProgress />
+        : (
           <Fragment>
-            <Source
-              id='bicyclePaths'
-              type="geojson"
-              data={dataFetch.data}
+            <MapGL
+              {...viewport}
+              mapboxApiAccessToken={token}
+              mapStyle='mapbox://styles/mapbox/streets-v11'
+              onViewportChange={onViewportChange}
+              onNativeClick={e => setCoordinates({latitude: e.lngLat[0], longitude: e.lngLat[1]})}
             >
-              <Layer
-                id='bicyclePaths'
-                source='bicyclePaths'
-                type='line'
-                paint={{
-                  'line-color': 'red',
-                  'line-width': 1
-                }}
-              />
-            </Source>
-            <Source
-              id='greenPaths'
-              type="geojson"
-              data={{...dataFetch.data, features: greenRoutes, totalFeatures: greenRoutes.length}}
-            >
-              <Layer
-                id='greenPaths'
-                source='greenPaths'
-                type='line'
-                paint={{
-                  'line-color': '#0E8C38',
-                  'line-width': 1
-                }}
-              />
-            </Source>
+              <Marker latitude={coordinates.latitude} longitude={coordinates.longitude} >
+                <FiberManualRecordIcon style={{ color: 'crimson', fontSize: 32, cursor: 'pointer' }} />
+              </Marker>
+              {dataFetch.data &&
+                <Fragment>
+                  <Source
+                    id='bicyclePaths'
+                    type="geojson"
+                    data={dataFetch.data}
+                  >
+                    <Layer
+                      id='bicyclePaths'
+                      source='bicyclePaths'
+                      type='line'
+                      paint={{
+                        'line-color': 'red',
+                        'line-width': 1.5
+                      }}
+                    />
+                  </Source>
+                  <Source
+                    id='greenPaths'
+                    type='geojson'
+                    data={{...dataFetch.data, features: greenRoutes, totalFeatures: greenRoutes.length}}
+                  >
+                    <Layer
+                      id='greenPaths'
+                      source='greenPaths'
+                      type='line'
+                      paint={{
+                        'line-color': '#0E8C38',
+                        'line-width': 1.5
+                      }}
+                    />
+                  </Source>
+                </Fragment>
+              }
+            </MapGL>
+            <DataChart latitude={coordinates.latitude} longitude={coordinates.longitude} />
           </Fragment>
-        }
-      </MapGL>
-      <DataChart latitude={coordinates.latitude} longitude={coordinates.longitude} />
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default Map
